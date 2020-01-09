6963c503f7b8551ffe8ca2c6c342a2b244e3614d
Rafael Marchesin <rmarchesin@gmail.com>
2020-01-09 17:24:56 -0300

    Acrescentados comentários de tarefas a serem feitas

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+/**
+ * =================== Tarefas =====================
+ * 1. Criar vetor para armazenar valores digitados
+ * 2. Configurar ação dos operadores
+ * 3. Criar função de cada OPERAÇÃO dos valores armazenados
+ * 4. 
+ * =================== Tarefas =====================
+ */
+
+ /**
+  * ============== Problema / Solução ==============
+  * 1.
+  * Problema: O número zero não é exibido na caixa de texto
+  * Solução: converter todos os valores que forem exibidos na
+  * caixa de texto para o tipo STRING. Salvar em formato número
+  * apenas no vetor destinado aos valores da operação.
+  * 
+  * 2.
+  * Problema: 
+  * Solução: 
+  * ============== Problema / Solução ==============
+ */
+
+
+//Foi preciso colocar a variável "valorConcatenado" no escopo global
 let valorConcatenado = 0
 
 function entradaDados(valorClicado){
@@ -18,4 +43,12 @@ function btnClicado(idElemento){
 
 function btnOperadores(){
     //Funcionalidades dos botões operadores
+
+    /**
+     * Na primeira versão da calculadora, ao clicar em qualquer operador,
+     * deve-se apagar o conteúdo da caixa de texto e o operador deverá ser
+     * utilizado para realizar a operação. ele não deve ser concatenado
+     * com os valores que já estão na caixa de exibição.
+    */
+
 }
